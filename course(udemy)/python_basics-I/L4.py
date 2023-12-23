f93fabfd4d8c638002a73127c82a3e9f616bf51d
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-24 00:24:40 +0530

    l4

---
+++ course(udemy)/python_basics-I/L4.py
@@ -0,0 +1,19 @@
+# Fundatamental Data Types
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+
+print(type(3+4))
+print(type(3-4))
+print(type(3*4))
+print(type(3/4))
+
+print(2**3) # 2^3
+print(5//4) # floor division
+
+print(type(3//4))
+
+print(6%4)
+print(type(6%4))
