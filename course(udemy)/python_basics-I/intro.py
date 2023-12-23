e5dfcf8f5e9e4d428039de7a45bc6c24008b50a9
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-24 00:03:09 +0530

    basic

---
+++ course(udemy)/python_basics-I/intro.py
@@ -0,0 +1,21 @@
+print("My name is yashraj")
+
+# input("What is your name? ")
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+# Fundamental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes --> custom types
+
+# Specialized data types --> modules
+None # null
