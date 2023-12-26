2fbc096ba457d7b1de5c4a78d98ccfb96f5fd175
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-26 21:44:46 +0530

    L15

---
+++ course(udemy)/python_basics-I/L13.py
@@ -0,0 +1,20 @@
+print(type("my string"))
+
+username = "supercoder"
+username = "supercoder"
+
+print(username)
+
+long_string = '''
+WOW
+0 0
+---
+'''
+print(long_string)
+
+first_name = "John"
+last_name = "Smith"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
