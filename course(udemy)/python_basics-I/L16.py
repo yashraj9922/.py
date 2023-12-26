f16903c6be866b5547113fca930000d07169d623
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-12-26 21:52:27 +0530

    L16

---
+++ course(udemy)/python_basics-I/L16.py
@@ -0,0 +1,15 @@
+#  Escape Sequences
+#  double quotes inside double quotes...single quotes inside single quotes
+
+# \ symbol is used to escape the character after it...it is used to print the character after it
+
+weather = " It\'s \"kind of\" sunny"
+print(weather)
+
+#  \t is used to print a tab
+weather = "\t It\'s \"kind of\" sunny"
+print(weather)
+
+#  \n is used to print a new line
+weather = "\t It\'s \"kind of\" sunny \n Hope you have a good day"
+print(weather)
